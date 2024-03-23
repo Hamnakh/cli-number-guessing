6236a5b7ec8bc06927c359b714f91ee439e66091
Hamna khan <160540710+Hamnakh@users.noreply.github.com>
2024-03-23 16:08:46 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([]);
+
+//1-compare will generate a random number
+//2-user input for gguessing number
+//3-compare user input with computer generated number and show result
+
+const randomNumber = Math.floor(Math.random()* 6+1);
+console.log("Welcome to number guessing game");
+const answers = await inquirer.prompt([
+    {
+    name: "User Guess Number",
+    type: "number",
+    message: "please guess a number between 1-6 :",
+    },
+]);
+console.log(answers);
+if (answers.userGuessNumber == randomNumber){
+    console.log("congratulations! you guessed right number");
+}else{
+    console.log("you guessed wrong number");
+}
